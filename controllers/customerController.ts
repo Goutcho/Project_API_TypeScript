44089e7f50c8c367dad19d39c5a986d6e2c734dc
GLacaille <60034772+Goutcho@users.noreply.github.com>
2023-09-21 23:47:17 +0200

    Add files via upload

--- controllers/customerController.ts
+++ controllers/customerController.ts
@@ -1,43 +1,68 @@
-import { MongoClient, Collection, Document } from "mongodb";
-import { Request, Response } from "express";
+// Importation des modules nécessaires
+import { MongoClient, Collection, Document } from "mongodb"; // TypeScript: Typage pour MongoDB
+import { Request, Response } from "express"; // TypeScript: Typage pour les objets Express
 
+// Initialisation de la connexion MongoDB
 const client = new MongoClient("mongodb://localhost:27017");
-
 async function connect() {
   await client.connect();
 }
-
 connect();
 
+// Initialisation de la base de données et de la collection
 const db = client.db("gameStore");
+const customersCollection: Collection<Document> = db.collection("customers"); // TypeScript: Typage de la collection
 
-const customersCollection: Collection<Document> = db.collection("customers");
-
-export const getCustomers = async (req: Request, res: Response): Promise<void> => {
+/**
+ * Récupère tous les clients de la collection "customers".
+ * @param req - La requête HTTP entrante.
+ * @param res - La réponse HTTP sortante.
+ * @returns {Promise<void>} - Renvoie une promesse vide.
+ */
+export const getCustomers = async (req: Request, res: Response): Promise<void> => { // TypeScript: Typage des paramètres et du retour
   const customers = await customersCollection.find({}).toArray();
   res.json(customers);
 };
 
-export const getCustomerById = async (req: Request, res: Response): Promise<void> => {
+/**
+ * Récupère un client spécifique par son identifiant.
+ * @param req - La requête HTTP entrante.
+ * @param res - La réponse HTTP sortante.
+ * @returns {Promise<void>} - Renvoie une promesse vide.
+ */
+export const getCustomerById = async (req: Request, res: Response): Promise<void> => { // TypeScript: Typage des paramètres et du retour
   const { id } = req.params;
   const customer = await customersCollection.findOne({ id: Number(id) });
   customer ? res.json(customer) : res.status(404).json({ message: "Customer not found" });
 };
 
-export const createCustomer = async (req: Request, res: Response): Promise<void> => {
+/**
+ * Crée un nouveau client dans la collection "customers".
+ * @param req - La requête HTTP entrante.
+ * @param res - La réponse HTTP sortante.
+ * @returns {Promise<void>} - Renvoie une promesse vide.
+ */
+export const createCustomer = async (req: Request, res: Response): Promise<void> => { // TypeScript: Typage des paramètres et du retour
   const newCustomer = req.body;
   await customersCollection.insertOne(newCustomer);
   res.status(201).json(newCustomer);
 };
 
-export const updateCustomer = async (req: Request, res: Response): Promise<void> => {
-  const id = req.params.id; 
-  const updatedCustomer: any = req.body;  
+/**
+ * Met à jour un client existant dans la collection "customers".
+ * @param req - La requête HTTP entrante.
+ * @param res - La réponse HTTP sortante.
+ * @returns {Promise<void>} - Renvoie une promesse vide.
+ */
+export const updateCustomer = async (req: Request, res: Response): Promise<void> => { // TypeScript: Typage des paramètres et du retour
+  const id = req.params.id;
+  const updatedCustomer: any = req.body; // TypeScript: Typage explicite comme 'any'
 
   delete updatedCustomer._id;
 
   const filter = { id: Number(id) };
   const update = { $set: updatedCustomer };
+
   const result = await customersCollection.updateOne(filter, update);
 
   if (result.matchedCount > 0) {
@@ -47,7 +72,13 @@ export const updateCustomer = async (req: Request, res: Response): Promise<void>
   }
 };
 
-export const deleteCustomer = async (req: Request, res: Response): Promise<void> => {
+/**
+ * Supprime un client spécifique par son identifiant.
+ * @param req - La requête HTTP entrante.
+ * @param res - La réponse HTTP sortante.
+ * @returns {Promise<void>} - Renvoie une promesse vide.
+ */
+export const deleteCustomer = async (req: Request, res: Response): Promise<void> => { // TypeScript: Typage des paramètres et du retour
   const { id } = req.params;
   const result = await customersCollection.deleteOne({ id: Number(id) });
   result.deletedCount > 0 ? res.status(204).send() : res.status(404).json({ message: "Customer not found" });
--- controllers/gameController.ts
+++ controllers/gameController.ts
@@ -1,38 +1,62 @@
-import { MongoClient, Collection, Document } from "mongodb";
-import { Request, Response } from "express";
+// Importation des modules nécessaires
+import { MongoClient, Collection, Document } from "mongodb"; // TypeScript: Typage pour MongoDB
+import { Request, Response } from "express"; // TypeScript: Typage pour les objets Express
 
+// Initialisation de la connexion MongoDB
 const client = new MongoClient("mongodb://localhost:27017");
-
 async function connect() {
   await client.connect();
 }
-
 connect();
 
+// Initialisation de la base de données et de la collection
 const db = client.db("gameStore");
+const gamesCollection: Collection<Document> = db.collection("games"); // TypeScript: Typage de la collection
 
-const gamesCollection: Collection<Document> = db.collection("games");
-
-export const getGames = async (req: Request, res: Response): Promise<void> => {
+/**
+ * Récupère tous les jeux de la collection "games".
+ * @param req - La requête HTTP entrante.
+ * @param res - La réponse HTTP sortante.
+ * @returns {Promise<void>} - Renvoie une promesse vide.
+ */
+export const getGames = async (req: Request, res: Response): Promise<void> => { // TypeScript: Typage des paramètres et du retour
   const games = await gamesCollection.find({}).toArray();
   res.json(games);
 };
 
-export const getGameById = async (req: Request, res: Response): Promise<void> => {
+/**
+ * Récupère un jeu spécifique par son identifiant.
+ * @param req - La requête HTTP entrante.
+ * @param res - La réponse HTTP sortante.
+ * @returns {Promise<void>} - Renvoie une promesse vide.
+ */
+export const getGameById = async (req: Request, res: Response): Promise<void> => { // TypeScript: Typage des paramètres et du retour
   const { id } = req.params;
   const game = await gamesCollection.findOne({ id: Number(id) });
   game ? res.json(game) : res.status(404).json({ message: "Game not found" });
 };
 
-export const createGame = async (req: Request, res: Response): Promise<void> => {
+/**
+ * Crée un nouveau jeu dans la collection "games".
+ * @param req - La requête HTTP entrante.
+ * @param res - La réponse HTTP sortante.
+ * @returns {Promise<void>} - Renvoie une promesse vide.
+ */
+export const createGame = async (req: Request, res: Response): Promise<void> => { // TypeScript: Typage des paramètres et du retour
   const newGame = req.body;
   await gamesCollection.insertOne(newGame);
   res.status(201).json(newGame);
 };
 
-export const updateGame = async (req: Request, res: Response): Promise<void> => {
+/**
+ * Met à jour un jeu existant dans la collection "games".
+ * @param req - La requête HTTP entrante.
+ * @param res - La réponse HTTP sortante.
+ * @returns {Promise<void>} - Renvoie une promesse vide.
+ */
+export const updateGame = async (req: Request, res: Response): Promise<void> => { // TypeScript: Typage des paramètres et du retour
   const id = req.params.id;
-  const updatedGame: any = req.body;
+  const updatedGame: any = req.body; // TypeScript: Typage explicite comme 'any'
 
   delete updatedGame._id;
 
@@ -48,8 +72,13 @@ export const updateGame = async (req: Request, res: Response): Promise<void> => 
   }
 };
 
-
-export const deleteGame = async (req: Request, res: Response): Promise<void> => {
+/**
+ * Supprime un jeu spécifique par son identifiant.
+ * @param req - La requête HTTP entrante.
+ * @param res - La réponse HTTP sortante.
+ * @returns {Promise<void>} - Renvoie une promesse vide.
+ */
+export const deleteGame = async (req: Request, res: Response): Promise<void> => { // TypeScript: Typage des paramètres et du retour
   const { id } = req.params;
   const result = await gamesCollection.deleteOne({ id: Number(id) });
   result.deletedCount > 0 ? res.status(204).send() : res.status(404).json({ message: "Game not found" });
